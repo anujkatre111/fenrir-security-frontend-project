de39deaece3c7766c2c8b9023f2ab530484cf28c
Anuj Katre <anujkatre72@gmail.com>
2026-03-03 22:01:48 +0530

    Scan Detail Page

--- src/pages/ScanDetail.jsx
+++ src/pages/ScanDetail.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Sidebar from '../components/layout/Sidebar'
+import Header from '../components/layout/Header'
+import ProgressDetailCard from '../components/scan/ProgressDetailCard'
 
 const ScanDetail = () => {
   return (
-    <div>
-      <Sidebar/>
+    <div className='flex'>
+      <Sidebar />
+      <div className='flex-1 flex flex-col min-h-screen'>
+        <Header />
+        <main className='flex-1 p-6'>
+          <ProgressDetailCard />
+        </main>
+      </div>
     </div>
   )
 }
